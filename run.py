8e62a72ba2701a0f3243f81b1b12ba2d575c0302
Alen Buhanec <a.buhanec@warwick.ac.uk>
2017-05-02 23:27:04 +0100

    Add web config

--- run.py
+++ run.py
@@ -2,11 +2,19 @@ from mcmp import get_mod
 import pandas as pd
 import yaml
 import sys
+import requests
 
 
 if __name__ == '__main__':
-    with open(sys.argv[1], 'r') as f:
-        mod_list = yaml.load(f)
+    if sys.argv[1].startswith('http'):
+        response = requests.get(sys.argv[1])
+        if response.ok:
+            mod_list = yaml.load(response.text)
+        else:
+            raise Exception(response.status_code)
+    else:
+        with open(sys.argv[1], 'r') as f:
+            mod_list = yaml.load(f)
 
     mods = {mod: get_mod(mod) for mod in mod_list}
     pd.set_option('display.max_rows', len(mods))
