8ff4ccb7b6e76ac86a3afc4c8da798aae8bcdeea
Alen Buhanec <a.buhanec@warwick.ac.uk>
2017-05-06 17:57:56 +0100

    download only on explicit demand

--- run.py
+++ run.py
@@ -7,23 +7,25 @@ import requests_cache
 
 
 if __name__ == '__main__':
-    if sys.argv[1].startswith('http'):
+    download = sys.argv[1] == '--download' or sys.argv[1] == '-d'
+
+    if sys.argv[-1].startswith('http'):
         with requests_cache.disabled():
-            response = requests.get(sys.argv[1])
+            response = requests.get(sys.argv[-1])
         if response.ok:
             mod_list = yaml.load(response.text)
         else:
             raise Exception(response.status_code)
     else:
-        with open(sys.argv[1], 'r') as f:
+        with open(sys.argv[-1], 'r') as f:
             mod_list = yaml.load(f)
 
-    mods = {mod: get_mod(mod) for mod in mod_list}
-    pd.set_option('display.max_rows', len(mods))
-    pd.set_option('display.width', 240)
+    mods = {mod: get_mod(mod, download=download) for mod in mod_list}
     df = pd.DataFrame(mods).T
     df.accepted = df.accepted.astype(bool)
 
+    pd.set_option('display.max_rows', len(mods))
+    pd.set_option('display.width', 240)
     print('Accepted:')
     print(df[df.accepted][['name', 'channel', 'filename', 'mod_ver']])
     print()
